1363f96dbad3fb54eceec5dc76eaf12b05f856da
lenmochen <lenmo_net@hotmail.com>
2020-02-06 18:37:38 +0800

    1.修改說明:輸出文字說明 2.調整說明:簡化程式結構，移除continue語法

--- 檢查回文日程式.py
+++ 檢查回文日程式.py
@@ -6,27 +6,32 @@ license: "MIT License"
 description: "判斷是否為回文日程式(簡易版)。"
 """
 
-slist = "\n請輸入檢查日期，例如:2020年1月13日，輸入20200113: "
-stag = "+"*20
+slist = "輸入檢查日期，例如:2020年1月13日，輸入20200113: "
+stag = "="*20
 print("\n" +stag +"程式開始" +stag )
+
+search_date = input( slist )
+print ("\n將查詢日期：",search_date)
 while True:
-    search_date = input( slist )
-    print ("\n將查詢日期：",search_date)
     chk_status = input("是否正確(y/n)：")
     if ( chk_status == "y" or chk_status == "Y" ):
         print("\n開始進行檢查作業...",end='')
         search_date_year = search_date[:4]
         search_date_mday = search_date[-1:-5:-1]
         if search_date_year == search_date_mday:
-            print("當天是回文日")
+            print("是回文日")
         else:
             print("不是回文日")
         print("\n" +stag +"程式結束" +stag)
         break
+
     elif ( chk_status == "n" or chk_status == "N" ):
-        continue
+        print ("\n重來一次：")
+        search_date = input( slist )
+        print ("\n將查詢日期：",search_date)
+
     else:
-        print("\n***需選擇 y|n ，請需重新輸入***\n")
+        print("\n◎ 請選擇 y|n")
 
 
 __author__ = "icoingithink <icoingithink@gmail.com>"
