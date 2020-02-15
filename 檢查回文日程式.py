5a3e63d6d5f2f63129967bd64086b8120eef605c
lenmochen <lenmo_net@hotmail.com>
2020-02-15 18:07:56 +0800

    1.調整程式寫法2.補上註解

--- 檢查回文日程式.py
+++ 檢查回文日程式.py
@@ -2,39 +2,39 @@
 # -*- coding: utf-8 -*-
 
 """
+author: "icoding.ithink <icoding.ithink@gmail.com>"
+copyright: "Copyright 2020, 海獅程式開源教材-Python"
 license: "MIT License"
-description: "判斷是否為回文日程式(簡易版)。"
+description: "判斷所輸入的日期，是否為回文日？"
 """
 
 slist = "輸入檢查日期，例如:2020年1月13日，輸入20200113: "
 stag = "="*20
 print("\n" +stag +"程式開始" +stag )
-
 search_date = input( slist )
-print ("\n將查詢日期：",search_date)
+
 while True:
+    print ("\n將查詢日期：",search_date)
     chk_status = input("是否正確(y/n)：")
+    
+    #輸入日期正確流程
     if ( chk_status == "y" or chk_status == "Y" ):
         print("\n開始進行檢查作業...",end='')
         search_date_year = search_date[:4]
         search_date_mday = search_date[-1:-5:-1]
         if search_date_year == search_date_mday:
-            print("是回文日")
+            print(" << 是回文日 >>")
         else:
-            print("不是回文日")
-        print("\n" +stag +"程式結束" +stag)
+            print(" << 不是回文日 >>")
         break
-
+    
+    #輸入日期錯誤流程
     elif ( chk_status == "n" or chk_status == "N" ):
         print ("\n重來一次：")
         search_date = input( slist )
-        print ("\n將查詢日期：",search_date)
-
+    
+    #輸入非y或n的流程
     else:
         print("\n◎ 請選擇 y|n")
 
-
-__author__ = "icoingithink <icoingithink@gmail.com>"
-__copyright__ = "Copyright 2020, 海獅程式開源教材-Python"
-__license__ = "MIT License"
-__description__ = "判斷是否為回文日程式(簡易版)。"
+print("\n" +stag +"程式結束" +stag)
